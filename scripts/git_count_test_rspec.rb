1370f89c60c0923c4f742839f5a7a6fb727ba36b
sue445 <go.sueyoshi@drecom.co.jp>
2012-08-01 12:05:47 +0900

    modify

--- scripts/git_count_test_rspec.rb
+++ scripts/git_count_test_rspec.rb
@@ -2,10 +2,16 @@
 
 user = `git config --get user.name`.strip
 until_commit = ARGV[0] || "master"
-testcase_regexp = "^\\+ *it "
+testcase_regexp = " *it "
 
-command = "git log --author=#{user} --remove-empty --oneline --unified=0 HEAD #{until_commit} | grep  -E '#{testcase_regexp}'"
+plus_command = "git log --author=#{user} --remove-empty --oneline --unified=0 --ignore-all-space HEAD #{until_commit} | grep -E '^\\+#{testcase_regexp}' -c"
+minus_command = "git log --author=#{user} --remove-empty --oneline --unified=0 --ignore-all-space HEAD #{until_commit} | grep -E '^-#{testcase_regexp}' -c"
 
-p command
-system(command)
-system(command + " -c")
+p plus_command
+p minus_command
+
+plus_count = `#{plus_command}`.strip.to_i
+minus_count = `#{minus_command}`.strip.to_i
+
+p "plus=#{plus_count}, minus=#{minus_count}, diff=#{plus_count - minus_count}"
+
