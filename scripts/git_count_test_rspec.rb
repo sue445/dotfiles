bc23e94dae34b546a52e1bb373b271e3432a6b56
sue445 <go.sueyoshi@drecom.co.jp>
2012-08-01 12:09:15 +0900

    refactor

--- scripts/git_count_test_rspec.rb
+++ scripts/git_count_test_rspec.rb
@@ -3,15 +3,15 @@
 user = `git config --get user.name`.strip
 until_commit = ARGV[0] || "master"
 testcase_regexp = " *it "
+git_log = "git log --author=#{user} --remove-empty --oneline --unified=0 --ignore-all-space HEAD #{until_commit}"
 
-plus_command = "git log --author=#{user} --remove-empty --oneline --unified=0 --ignore-all-space HEAD #{until_commit} | grep -E '^\\+#{testcase_regexp}' -c"
-minus_command = "git log --author=#{user} --remove-empty --oneline --unified=0 --ignore-all-space HEAD #{until_commit} | grep -E '^-#{testcase_regexp}' -c"
+p git_log
 
-p plus_command
-p minus_command
+plus_grep = "#{git_log} | grep -E '^\\+#{testcase_regexp}' -c"
+minus_grep = "#{git_log} | grep -E '^-#{testcase_regexp}' -c"
 
-plus_count = `#{plus_command}`.strip.to_i
-minus_count = `#{minus_command}`.strip.to_i
+plus_count = `#{plus_grep}`.strip.to_i
+minus_count = `#{minus_grep}`.strip.to_i
 
 p "plus=#{plus_count}, minus=#{minus_count}, diff=#{plus_count - minus_count}"
 
