99ce8f60236057d8fa5430365995f5bc4d92d8ef
sue445 <go.sueyoshi@drecom.co.jp>
2012-08-06 12:03:46 +0900

    support 'its'

--- scripts/git_count_test_rspec.rb
+++ scripts/git_count_test_rspec.rb
@@ -2,13 +2,16 @@
 
 user = `git config --get user.name`.strip
 since_commit = ARGV[0] || "master"
-testcase_regexp = " *it "
+testcase_regexp = "\s*its?"
 git_log = "git log --author=#{user} --remove-empty --oneline --unified=0 --ignore-all-space #{since_commit}..HEAD"
 
 p git_log
 
 plus_grep = "#{git_log} | grep -E '^\\+#{testcase_regexp}' -c"
 minus_grep = "#{git_log} | grep -E '^-#{testcase_regexp}' -c"
+
+#p plus_grep
+#p minus_grep
 
 plus_count = `#{plus_grep}`.strip.to_i
 minus_count = `#{minus_grep}`.strip.to_i
