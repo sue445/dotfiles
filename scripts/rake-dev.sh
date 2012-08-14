73c7cb5d8b0a783861d6b0813811f1c230917e63
sue445 <go.sueyoshi@drecom.co.jp>
2012-08-14 10:42:40 +0900

    refactor: log format

--- scripts/rake-dev.sh
+++ scripts/rake-dev.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "[RAILS_ENV=development]"
+echo "[START] rake $1 RAILS_ENV=development"
 rake $1 RAILS_ENV=development
+echo "[END]   rake $1 RAILS_ENV=development"
 
-echo "[RAILS_ENV=test]"
+echo "[START] rake $1 RAILS_ENV=test"
 rake $1 RAILS_ENV=test
+echo "[END]   rake $1 RAILS_ENV=test"
+
