7d810e0fcb4d79e0b4ddf5601e682b8895c5268f
sue445 <go.sueyoshi@drecom.co.jp>
2012-08-23 16:52:15 +0900

    add comment

--- scripts/rake-dev.sh
+++ scripts/rake-dev.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# ex)
+# vi ~/.bash_profile
+#
+# alias rake-dev='~/dotfiles/scripts/rake-dev.sh'
+
 echo "[START] rake $1 RAILS_ENV=development"
 rake $1 RAILS_ENV=development
 echo "[END]   rake $1 RAILS_ENV=development"
