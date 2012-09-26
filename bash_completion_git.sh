ca333e046a09653af7b2f0bf84bc532ed908f0fc
sue445 <go.sueyoshi@drecom.co.jp>
2012-09-26 09:38:55 +0900

    edit bash_completion color
    current dir name: blue -> yellow

--- bash_completion_git.sh
+++ bash_completion_git.sh
@@ -9,5 +9,5 @@ source /usr/local/etc/bash_completion.d/git-prompt.sh
 source /usr/local/etc/bash_completion.d/git-completion.bash
 GIT_PS1_SHOWDIRTYSTATE=true
 #export PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
-export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
+export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[33m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
 
