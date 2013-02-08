2752239905208044e31f79723e82593763c0e235
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2013-02-08 09:52:51 +0900

    support file

--- scripts/rm_xttr.rb
+++ scripts/rm_xttr.rb
@@ -3,9 +3,14 @@
 # Macでダウンロードしたファイルにつくアットマークを再帰的に削除する
 # ref. http://d.hatena.ne.jp/kanonji/20100912/1284292517
 
-dir = ARGV[0]
+path = ARGV[0]
 
-Dir.glob("#{dir}/**/*").each do |file|
-  `xattr -d com.apple.metadata:kMDItemWhereFroms #{file}`
-  `xattr -d com.apple.quarantine #{file}`
+if File.directory?(path)
+  Dir.glob("#{path}/**/*").each do |file|
+    `xattr -d com.apple.metadata:kMDItemWhereFroms #{file}`
+    `xattr -d com.apple.quarantine #{file}`
+  end
+else
+  `xattr -d com.apple.metadata:kMDItemWhereFroms #{path}`
+  `xattr -d com.apple.quarantine #{path}`
 end
