aa089854e46c0cf28dfa634b654cdc628ff980b6
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2014-06-26 14:43:13 +0900

    Add tmux conf link

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ ln -s ~/dotfiles/_bashrc    ~/.bashrc
 ln -s ~/dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/dotfiles/_gitignore ~/.gitignore
 ln -s ~/dotfiles/_tigrc     ~/.tigrc
+ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 
 cp -i ~/dotfiles/_gitconfig.local.example ~/.gitconfig.local
