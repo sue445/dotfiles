97db5b51514f151114d076f2f2f812465fe314c3
sue445 <sue445@sue445.net>
2015-05-28 18:23:45 +0900

    support both rails and padrino

--- scripts/rake-dev.sh
+++ scripts/rake-dev.sh
@@ -1,21 +1,11 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 # ex)
 # vi ~/.bash_profile
 #
 # alias rake-dev='~/dotfiles/scripts/rake-dev.sh'
 
-echo "[START] bundle exec rake $1 RAILS_ENV=development"
-bundle exec rake $1 RAILS_ENV=development
-
-RET=$?
-if [ $RET -ne 0 ]; then
-  exit $RET
-fi
-
-echo "[END]   bundle exec rake $1 RAILS_ENV=development"
-
-echo "[START] bundle exec rake $1 RAILS_ENV=test"
-bundle exec rake $1 RAILS_ENV=test
-echo "[END]   bundle exec rake $1 RAILS_ENV=test"
+# support both rails and padrino
+RAILS_ENV=development RACK_ENV=development bundle exec rake $1
+RAILS_ENV=test RACK_ENV=test bundle exec rake $1
 
