2a296723be465f789c9313b2dfae08234f6a86a6
sue445 <sue445@sue445.net>
2015-05-29 14:48:44 +0900

    bugfix link

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ ln -s ~/dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/dotfiles/_gitignore ~/.gitignore
 ln -s ~/dotfiles/_tigrc     ~/.tigrc
 ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/_vim       ~/.vim
+ln -s ~/dotfiles/vimfiles   ~/.vim
 ln -s ~/dotfiles/_vimrc     ~/.vimrc
 ln -s ~/dotfiles/_rbenv/default-gems ~/.rbenv/default-gems
 
