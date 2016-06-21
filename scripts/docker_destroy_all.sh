6e5c2909aa74ed8e68924dc80b8f80b0fce80bfc
sue445 <sue445@sue445.net>
2016-06-21 23:16:45 +0900

    Add docker_destroy_all.sh

---
+++ scripts/docker_destroy_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+
+# via. https://gist.github.com/JeffBelback/5687bb02f3618965ca8f
+
+# Stop all containers
+docker stop $(docker ps -a -q)
+# Delete all containers
+docker rm $(docker ps -a -q)
+# Delete all images
+docker rmi $(docker images -q)
