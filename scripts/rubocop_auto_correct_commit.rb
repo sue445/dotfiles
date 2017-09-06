17d3495b03e976eeaa30fbf7fa400e4d91fcca7b
sue445 <sue445@sue445.net>
2017-09-06 19:31:08 +0900

    Add COP_ORDERS

--- scripts/rubocop_auto_correct_commit.rb
+++ scripts/rubocop_auto_correct_commit.rb
@@ -3,13 +3,25 @@
 
 require "json"
 
+DEFAULT_ORDER = 100
+
+# --auto-correctする時の実行順(数字が小さい方が先に実行される)
+COP_ORDERS = {
+  # Style/UnneededPercentQはStyle/StringLiteralsのようにシングルクオーテーション優先かダブルクオーテーション優先のような
+  # configがないので、Style/StringLiteralsでダブルクオーテーションにした後でStyle/UnneededPercentQでシングルクオーテーション
+  # になると二度手間になってしまうので先にStyle/UnneededPercentQを実行した方がいい
+  "Style/UnneededPercentQ" => 50,
+}.tap { |hash| hash.default = DEFAULT_ORDER }
+
 rubocop_result = JSON.parse(`rubocop --format=json`)
 
 cop_names = []
 rubocop_result["files"].each do |file|
   cop_names += file["offenses"].map { |offense| offense["cop_name"] }
 end
-cop_names = cop_names.sort.uniq
+
+# 「第1ソートキー:COP_ORDERSに登録されてる実行順、第2ソートキー:cop名」の順でソートする
+cop_names = cop_names.uniq.sort_by { |cop_name| [COP_ORDERS[cop_name], cop_name] }
 
 cop_names.each do |cop_name|
   # Depending on the execution order, there is a possibility that the offences may increase.
