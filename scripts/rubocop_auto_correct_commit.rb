5e48192925778d0178c96684213e00605eae2e05
sue445 <sue445@sue445.net>
2017-09-29 17:35:14 +0900

    Tweak run twice

--- scripts/rubocop_auto_correct_commit.rb
+++ scripts/rubocop_auto_correct_commit.rb
@@ -14,23 +14,24 @@ COP_ORDERS = {
   "Style/UnneededPercentQ" => 50,
 }.tap { |hash| hash.default = DEFAULT_ORDER }
 
-rubocop_result = JSON.parse(`rubocop --format=json`)
+# Depending on the execution order, there is a possibility that the offences may increase.
+# So it is executed twice
+2.times do
+  rubocop_result = JSON.parse(`rubocop --format=json`)
 
-cop_names = []
-rubocop_result["files"].each do |file|
-  cop_names += file["offenses"].map { |offense| offense["cop_name"] }
-end
+  cop_names = []
+  rubocop_result["files"].each do |file|
+    cop_names += file["offenses"].map { |offense| offense["cop_name"] }
+  end
 
-# 「第1ソートキー:COP_ORDERSに登録されてる実行順、第2ソートキー:cop名」の順でソートする
-cop_names = cop_names.uniq.sort_by { |cop_name| [COP_ORDERS[cop_name], cop_name] }
+  # 「第1ソートキー:COP_ORDERSに登録されてる実行順、第2ソートキー:cop名」の順でソートする
+  cop_names = cop_names.uniq.sort_by { |cop_name| [COP_ORDERS[cop_name], cop_name] }
 
-cop_names.each do |cop_name|
-  # Depending on the execution order, there is a possibility that the offences may increase.
-  # So it is executed twice
-  2.times do
+  cop_names.each do |cop_name|
     command = "rubocop --auto-correct --only #{cop_name}"
 
     ret = system command
     system "git commit -am ':cop: #{command}'" if ret
   end
 end
+
