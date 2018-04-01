49a6c2ecae5aa16e565aebf94b015216f7446012
sue445 <sue445@sue445.net>
2018-04-01 15:15:42 +0900

    Add ruby_update.sh

---
+++ scripts/ruby_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+readonly BEFORE_VERSION=$1
+readonly AFTER_VERSION=$2
+
+if [ -z "$BEFORE_VERSION" ] || [ -z "$AFTER_VERSION" ]; then
+  echo "Usage: ruby_update.sh <BEFORE_VERSION> <AFTER_VERSION>"
+  exit 1
+fi
+
+if sed --version 2>/dev/null | grep -q GNU; then
+  git grep -l ${BEFORE_VERSION} | grep -v Gemfile.lock | grep -e .ruby-version -e Gemfile -e wercker.yml -e .circleci/config.yml | xargs sed -i "s/${BEFORE_VERSION}/${AFTER_VERSION}/g"
+else
+  git grep -l ${BEFORE_VERSION} | grep -v Gemfile.lock | grep -e .ruby-version -e Gemfile -e wercker.yml -e .circleci/config.yml | xargs sed -i "" "s/${BEFORE_VERSION}/${AFTER_VERSION}/g"
+fi
+
+git commit -am "Upgrade to ruby ${AFTER_VERSION} :gem:"
