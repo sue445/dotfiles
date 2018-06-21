014e7b4b08631f6bd22afed9e313be56c97e0e48
sue445 <sue445@sue445.net>
2018-06-21 12:19:28 +0900

    Clean tar.gz

--- mac/install_mitamae.sh
+++ mac/install_mitamae.sh
@@ -2,6 +2,8 @@
 
 readonly MITAMAE_VERSION="1.5.6"
 
+rm -f bin/*
+
 cd bin/
 wget https://github.com/itamae-kitchen/mitamae/releases/download/v${MITAMAE_VERSION}/mitamae-x86_64-darwin.tar.gz
 tar -zxf mitamae-x86_64-darwin.tar.gz
