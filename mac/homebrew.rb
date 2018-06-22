a52e4a22a5d26b01a1307ae42734fb93401015fa
sue445 <sue445@sue445.net>
2018-06-23 05:14:34 +0900

    Remove legacy tap

--- mac/homebrew.rb
+++ mac/homebrew.rb
@@ -2,6 +2,7 @@ node[:homebrew][:package_names].each do |name|
   package name
 end
 
+node[:homebrew][:tap_names] ||= []
 node[:homebrew][:tap_names].each do |name|
   execute "brew tap #{name}" do
     not_if "brew tap | grep -q '#{name}'"
