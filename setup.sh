685335865daeef8baffb34e8f91231d3d8486659
sue445 <sue445@sue445.net>
2021-08-28 13:51:47 +0900

    Default to use _tmux_3_x.conf

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ ln -s ~/dotfiles/_bashrc    ~/.bashrc
 ln -s ~/dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/dotfiles/_gitignore ~/.gitignore
 ln -s ~/dotfiles/_tigrc     ~/.tigrc
-ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/_tmux_3_x.conf ~/.tmux.conf
 ln -s ~/dotfiles/vimfiles   ~/.vim
 ln -s ~/dotfiles/_vimrc     ~/.vimrc
 ln -s ~/dotfiles/_rbenv/default-gems ~/.rbenv/default-gems
