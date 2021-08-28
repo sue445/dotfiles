bd79754e1d89a8ea852052be721c29dbe5143282
sue445 <sue445@sue445.net>
2021-08-28 13:55:03 +0900

    :lipstick:

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
-ln -s ~/dotfiles/_agignore  ~/.agignore
-ln -s ~/dotfiles/_bashrc    ~/.bashrc
-ln -s ~/dotfiles/_gitconfig ~/.gitconfig
-ln -s ~/dotfiles/_gitignore ~/.gitignore
-ln -s ~/dotfiles/_tigrc     ~/.tigrc
+ln -s ~/dotfiles/_agignore      ~/.agignore
+ln -s ~/dotfiles/_bashrc        ~/.bashrc
+ln -s ~/dotfiles/_gitconfig     ~/.gitconfig
+ln -s ~/dotfiles/_gitignore     ~/.gitignore
+ln -s ~/dotfiles/_tigrc         ~/.tigrc
 ln -s ~/dotfiles/_tmux_3_x.conf ~/.tmux.conf
-ln -s ~/dotfiles/vimfiles   ~/.vim
-ln -s ~/dotfiles/_vimrc     ~/.vimrc
+ln -s ~/dotfiles/vimfiles       ~/.vim
+ln -s ~/dotfiles/_vimrc         ~/.vimrc
 
 # ln -s ~/dotfiles/_rbenv/default-gems ~/.rbenv/default-gems
 
