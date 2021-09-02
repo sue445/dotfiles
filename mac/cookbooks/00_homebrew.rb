fb453b45ca7bbd8d0e7cf509cf5734a1c9812731
sue445 <sue445@sue445.net>
2021-09-02 22:45:26 +0900

    Speed up 00_homebrew.rb :dash:

--- mac/cookbooks/00_homebrew.rb
+++ mac/cookbooks/00_homebrew.rb
@@ -8,9 +8,23 @@ node[:homebrew][:tap_names].each do |name|
   end
 end
 
+formula_installed_packages = `brew list -1 --formula`.each_line.map(&:strip)
+cask_installed_packages    = `brew list -1 --cask`.each_line.map(&:strip)
+
+# NOTE: mruby contains Set
+all_installed_packages = (formula_installed_packages + cask_installed_packages).each_with_object({}) do |package_name, hash|
+  package_name = package_name.gsub(/@.+$/, "")
+  hash[package_name] = true
+end
+
 node[:homebrew][:package_names].each do |name|
-  package name do
-    not_if "brew list | grep -E '^#{name}(@.+)?$'"
+  # NOTE: this is very slow!
+  # package name do
+  #   not_if "brew list | grep -E '^#{name}(@.+)?$'"
+  # end
+
+  unless all_installed_packages[name]
+    package name
   end
 end
 
